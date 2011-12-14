948475da026f4b0f72a5969e585e0a7b96e27246
Jacob Buck <buck.jacob@gmail.com>
2011-12-14 16:08:45 +1300

    Initial commit.

---
+++ usersonly.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Users Only
+Description: Restricts a Wordpress website to logged in users only and redirects unauthorised visitors to login page.
+Version: 1.0
+Author: Jacob Buck
+Author URI: http://jacobbuck.co.nz/
+*/
+
+class UsersOnly {
+	
+	function __construct () {
+		/* Register functions */
+		add_action("template_redirect", array($this,"template_redirect"));
+		add_action("login_head", array($this,"login_head"));
+	}
+	
+	function login_head () {
+		/* Hides back to blog link */
+		echo "<style type=\"text/css\" media=\"screen\">#backtoblog{display:none}</style>\n";
+	}
+	
+	function template_redirect () {
+		/* Redirects unauthorised visitors to login page */
+		if (! is_user_logged_in()) {
+			wp_redirect(wp_login_url(home_url()),302);
+			exit;
+		}
+	}
+	
+}
+
+$usersonly = new UsersOnly;
