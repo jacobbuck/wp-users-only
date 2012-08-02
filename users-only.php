1cf9f697fce391c8e1f6816d41cf1ed523dd21e2
Jacob Buck <buck.jacob@gmail.com>
2012-08-02 15:45:45 +1200

    v1.2.1 - Changed holding template file dir. Small fix in css.

--- users-only.php
+++ users-only.php
@@ -3,7 +3,7 @@
 Plugin Name: Users Only
 Plugin URI: https://github.com/jacobbuck/wp-users-only
 Description: Restrict a website to logged in users. Visitors get redirected to login page or displayed a holding page.
-Version: 1.2
+Version: 1.2.1
 Author: Jacob Buck
 Author URI: http://jacobbuck.co.nz/
 */
@@ -18,7 +18,7 @@ class Users_Only {
 	
 	function login_head () {
 		/* Hide back to blog link */
-		echo '<style>#backtoblog{display:none}</style>\n';
+		echo '<style>#backtoblog{display:none}</style>';
 	}
 	
 	function template_redirect () {
@@ -27,7 +27,7 @@ class Users_Only {
 			return;
 		
 		/* Show holding template file if is located, otherwise redirect to login page */	
-		if ( ! locate_template( array( 'holding.php' ), true, false ) ) 
+		if ( ! locate_template( array('holding/index.php'), true, false ) ) 
 			wp_redirect( wp_login_url( home_url() ), 302 );
 		
 		/* Shutdown */
